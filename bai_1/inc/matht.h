68f06891c054cf0d42415a11c88e919f071d4bbb
KhanhNguyencxht <nguyendinhkhanhcxht@gmail.com>
2023-12-02 22:34:00 +0700

    static_lib shared_lib

--- bai_1/inc/matht.h
+++
@@ -1,3 +0,0 @@
-#include<stdio.h>
-
-int sum(int a, int b);
--- bai_1/main.c
+++
@@ -1,9 +0,0 @@
-#include"matht.h"
-#include"hello.h"
-
-int main(){
-
-    hello();
-    printf("4 + 5 = %d", sum(4, 5));
-    return 0;
-}
